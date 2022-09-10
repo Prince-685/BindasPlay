18a12c0c5fd07c20f491f6f9f8d803a925be8572
Prince-685 <gprince685@gmail.com>
2022-09-11 02:13:50 +0530

    revert changes issue remains

--- authentication/models.py
+++ authentication/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from django.forms import ModelForm
 
-class Text(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-class TextForm(ModelForm):
-    class Meta:
-        model = Text
-        fields = '__all__'
+# Create your models here.
