e794c2e6b04e581395ad606d634e7ece13011ce1
Prince-685 <gprince685@gmail.com>
2022-09-11 12:59:57 +0530

    set debug to false

--- BindasPlay/settings.py
+++ BindasPlay/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#z$#+%496%n!(z=xnn0z%#2h)-mf#zwcsa3_3t&p)pef2lr#&v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "bindasplaywin.in", "www.bindasplaywin.in", "admin.bindasplaywin.in"]
 
